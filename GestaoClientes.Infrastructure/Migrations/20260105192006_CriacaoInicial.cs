a59efb7e970f73ec6fcd3887fa2cba65562d7e2a
Bruno Gonçalves <bruno.goncalves1999@hotmail.com>
2026-01-05 16:20:55 -0300

    migration e ajuste API

---
+++ GestaoClientes.Infrastructure/Migrations/20260105192006_CriacaoInicial.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestaoClientes.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriacaoInicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NomeFantasia = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Cnpj = table.Column<string>(type: "char(14)", maxLength: 14, nullable: false),
+                    Ativo = table.Column<bool>(type: "bit", nullable: false),
+                    DataCadastro = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DataAtualizacao = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clientes_Ativo",
+                table: "Clientes",
+                column: "Ativo");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clientes_Cnpj",
+                table: "Clientes",
+                column: "Cnpj",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clientes_NomeFantasia",
+                table: "Clientes",
+                column: "NomeFantasia");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+        }
+    }
+}
