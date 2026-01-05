3367ac9863f743e2a3481e7184a45544b90ee3a2
Bruno Gonçalves <bruno.goncalves1999@hotmail.com>
2026-01-05 14:45:00 -0300

    parcial

---
+++ GestaoClientes.Application/Queries/ObtemClientePorIdQueryHandler.cs
@@ -0,0 +1,47 @@
+﻿using GestaoClientes.Application.Common.Results;
+using GestaoClientes.Application.DTOs;
+using GestaoClientes.Domain.Interfaces;
+using MediatR;
+
+namespace GestaoClientes.Application.Queries
+{
+    public class ObtemClientePorIdQueryHandler : IRequestHandler<ObtemClientePorIdQuery, Result<ClienteDto>>
+    {
+        private readonly IClienteRepository _clienteRepository;
+
+        public ObtemClientePorIdQueryHandler(IClienteRepository clienteRepository)
+        {
+            _clienteRepository = clienteRepository;
+        }
+
+        public async Task<Result<ClienteDto>> Handle(ObtemClientePorIdQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var cliente = await _clienteRepository.ObterPorIdAsync(request.Id, cancellationToken);
+
+                if (cliente is null)
+                {
+                    return Result<ClienteDto>.Falha("Cliente não encontrado.");
+                }
+
+                var clienteDto = new ClienteDto
+                {
+                    Id = cliente.Id,
+                    NomeFantasia = cliente.NomeFantasia,
+                    Cnpj = cliente.Cnpj.Valor,
+                    CnpjFormatado = cliente.Cnpj.ObterFormatado(),
+                    Ativo = cliente.Ativo,
+                    DataCadastro = cliente.DataCadastro,
+                    DataAtualizacao = cliente.DataAtualizacao
+                };
+
+                return Result<ClienteDto>.Ok(clienteDto, "Cliente obtido com sucesso.");
+            }
+            catch (Exception ex)
+            {
+                return Result<ClienteDto>.Falha($"Erro ao obter cliente: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ GestaoClientes.Application/Queries/ObtemClientePorIdQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace GestaoClientes.Application.Queries
+{
+    public class ObtemClientePorIdQueryValidator : AbstractValidator<ObtemClientePorIdQuery>
+    {
+        public ObtemClientePorIdQueryValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty()
+                .WithMessage("O ID do cliente é obrigatório.");
+        }
+    }
+}
